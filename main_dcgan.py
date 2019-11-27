8bcc564c7627b153a2cfe3bfa0f0c18e527f4287
Herman Prawiro <herman.prawiro@gmail.com>
2019-11-28 01:24:48 +0800

    refactor dcgan

--- main_dcgan.py
+++ main_dcgan.py
@@ -19,31 +19,29 @@ parser.add_argument("--batch_size", type=int, default=64)
 parser.add_argument("--learning_rate", type=float, default=2e-4)
 parser.add_argument("--beta1", type=float, default=0.5)
 parser.add_argument("--beta2", type=float, default=0.999)
+parser.add_argument("--ndf", type=int, default=64, help="Base features multiplier for discriminator")
+parser.add_argument("--ngf", type=int, default=64, help="Base features multiplier for generator")
 parser.add_argument("--n_workers", type=int, default=8)
 parser.add_argument("--latent_dim", type=int, default=100)
 parser.add_argument("--image_ch", type=int, default=3)
-parser.add_argument("--checkpoint_path", type=str, default="checkpoints/dcgan")
-parser.add_argument("--result_path", type=str, default="results/dcgan")
+parser.add_argument("--image_res", type=int, default=64)
+parser.add_argument("--checkpoint_path", type=str, default="checkpoints")
+parser.add_argument("--result_path", type=str, default="results")
+parser.add_argument("--save_name", type=str, default="dcgan")
 parser.add_argument("--data_root", type=str, default=R"E:\Datasets\CelebA")
-
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
-        nn.init.normal_(m.weight.data, 1.0, 0.02)
-        nn.init.constant_(m.bias.data, 0)
 
 def main(args):
     if device.type == 'cuda':
         torch.backends.cudnn.benchmark = True
 
+    args.checkpoint_path = os.path.join(args.checkpoint_path, args.save_name)
+    args.result_path = os.path.join(args.result_path, args.save_name)
     os.makedirs(args.checkpoint_path, exist_ok=True)
     os.makedirs(args.result_path, exist_ok=True)
 
     tfs = transforms.Compose([
-        transforms.Resize(64),
-        transforms.CenterCrop(64),
+        transforms.Resize(args.image_res),
+        transforms.CenterCrop(args.image_res),
         transforms.ToTensor(),
         transforms.Normalize([0.5]*3, [0.5]*3)
     ])
@@ -51,17 +49,15 @@ def main(args):
     dataset = torchvision.datasets.CelebA(args.data_root, split="all", transform=tfs)
     dataloader = DataLoader(dataset, batch_size=args.batch_size, num_workers=args.n_workers, shuffle=True, pin_memory=True)
 
-    netG = dcgan_specnorm.Generator(latent_dim=args.latent_dim, out_dim=args.image_ch).to(device)
-    netD = dcgan_specnorm.Discriminator(in_dim=args.image_ch).to(device)
-    netG.apply(weights_init)
-    netD.apply(weights_init)
+    netG = dcgan.Generator(z_dim=args.latent_dim, ngf=args.ngf, img_dim=args.image_ch, resolution=args.image_res).to(device)
+    netD = dcgan.Discriminator(ndf=args.ndf, img_dim=args.image_ch, resolution=args.image_res).to(device)
 
     optG = torch.optim.Adam(netG.parameters(), lr=4e-4, betas=(args.beta1, args.beta2))
     optD = torch.optim.Adam(netD.parameters(), lr=1e-4, betas=(args.beta1, args.beta2))
 
-    criterion = GANLoss('vanilla', target_real_label=0.0, target_fake_label=1.0, target_fake_G_label=0.0).to(device)
+    criterion = GANLoss('vanilla', target_real_label=0.9, target_fake_label=0.0, target_fake_G_label=0.9).to(device)
 
-    fixed_noise = torch.randn(args.batch_size, args.latent_dim, device=device)
+    fixed_noise = torch.randn(64, args.latent_dim, device=device)
 
     netG.train()
     netD.train()
@@ -100,7 +96,7 @@ def main(args):
                 print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
                     % (epoch + 1, args.n_epochs, i, len(dataloader), lossD.item(), lossG.item(), Dx, Dgz1, Dgz2))
 
-            if i % 200 == 0:
+            if i % 50 == 0:
                 outG = netG(fixed_noise).detach()
                 save_image(outG, '%s/fake_epoch%03d_%04d.jpg' % (args.result_path, epoch + 1, i + 1))
         save_model((netG, netD), (optG, optD), epoch, args.checkpoint_path)            
