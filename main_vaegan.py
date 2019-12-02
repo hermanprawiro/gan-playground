31f6f541ff64d9363a53e07c8423ea3fbb149d61
Herman Prawiro <herman.prawiro@gmail.com>
2019-12-02 19:03:09 +0800

    vaegan

---
+++ main_vaegan.py
@@ -0,0 +1,172 @@
+import argparse
+import os
+
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torchvision.utils import save_image
+
+from models import vaegan
+from utils.criterion import GANLoss
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--n_epochs", type=int, default=100)
+parser.add_argument("--batch_size", type=int, default=128)
+parser.add_argument("--learning_rate", type=float, default=2e-4)
+parser.add_argument("--beta1", type=float, default=0.5)
+parser.add_argument("--beta2", type=float, default=0.999)
+parser.add_argument("--ndf", type=int, default=64, help="Base features multiplier for discriminator")
+parser.add_argument("--ngf", type=int, default=64, help="Base features multiplier for generator")
+parser.add_argument("--n_disc_update", type=int, default=1)
+parser.add_argument("--n_gen_update", type=int, default=1)
+parser.add_argument("--n_workers", type=int, default=8)
+parser.add_argument("--latent_dim", type=int, default=100)
+parser.add_argument("--image_ch", type=int, default=3)
+parser.add_argument("--image_res", type=int, default=64)
+parser.add_argument("--checkpoint_path", type=str, default="checkpoints")
+parser.add_argument("--result_path", type=str, default="results")
+parser.add_argument("--save_name", type=str, default="vaegan")
+parser.add_argument("--data_root", type=str, default=R"E:\Datasets\CelebA")
+
+def main(args):
+    if device.type == 'cuda':
+        torch.backends.cudnn.benchmark = True
+
+    args.checkpoint_path = os.path.join(args.checkpoint_path, args.save_name)
+    args.result_path = os.path.join(args.result_path, args.save_name)
+    os.makedirs(args.checkpoint_path, exist_ok=True)
+    os.makedirs(os.path.join(args.result_path, 'fake'), exist_ok=True)
+    os.makedirs(os.path.join(args.result_path, 'real'), exist_ok=True)
+
+    tfs = transforms.Compose([
+        transforms.Resize(args.image_res),
+        transforms.CenterCrop(args.image_res),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5]*args.image_ch, [0.5]*args.image_ch)
+    ])
+
+    dataset = torchvision.datasets.CelebA(args.data_root, split="all", transform=tfs, download=True)
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, num_workers=args.n_workers, shuffle=True, pin_memory=True)
+
+    netG = vaegan.Generator(z_dim=args.latent_dim, ngf=args.ngf, img_dim=args.image_ch, resolution=args.image_res).to(device)
+    netD = vaegan.Discriminator(z_dim=args.latent_dim, ndf=args.ndf, img_dim=args.image_ch, resolution=args.image_res).to(device)
+    netE = vaegan.Encoder(ndf=args.ndf, img_dim=args.image_ch, resolution=args.image_res, output_dim=args.latent_dim).to(device)
+
+    optG = torch.optim.Adam(netG.parameters(), lr=4e-4, betas=(args.beta1, args.beta2))
+    optD = torch.optim.Adam(netD.parameters(), lr=1e-4, betas=(args.beta1, args.beta2))
+    optE = torch.optim.Adam(netE.parameters(), lr=4e-4, betas=(args.beta1, args.beta2))
+
+    criterion_gan = GANLoss('vanilla', target_real_label=0.9, target_fake_label=0.0, target_fake_G_label=0.9).to(device)
+    criterion_sim = nn.BCELoss()
+
+    fixed_noise = torch.randn(32, args.latent_dim, device=device)
+    fixed_real = next(iter(dataloader))[0][:32].to(device)
+
+    netG.train()
+    netD.train()
+    netE.train()
+    for epoch in range(args.n_epochs):
+        for i, (x_real, _) in enumerate(dataloader):
+            x_real = x_real.to(device)
+
+            mu, logvar = netE.encode(x_real)
+            z_enc = netE.reparameterize(mu, logvar)
+            x_recon = netG(z_enc)
+            
+            z_prior = torch.randn(x_real.shape[0], args.latent_dim, device=device)
+            x_prior = netG(z_prior)
+            
+            # Encoder
+            optE.zero_grad()
+            lossE_prior = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+            _, featD_real = netD(x_real, True)
+            _, featD_recon = netD(x_recon, True)
+            loss_similarity = (featD_real - featD_recon).pow(2).mean()
+            lossE = lossE_prior + loss_similarity
+            lossE.backward()
+            optE.step()
+
+            # Generator
+            optG.zero_grad()
+            # Detach backprop to Encoder
+            x_recon = netG(z_enc.detach())
+            _, featD_real = netD(x_real, True)
+            _, featD_recon = netD(x_recon, True)
+            loss_similarity = (featD_real - featD_recon).pow(2).mean()
+
+            outD = netD(x_prior)
+            Dgz1 = outD.mean().item()
+            lossG_prior = criterion_gan(outD, False, True)
+            outD = netD(x_recon)
+            Dgz2 = outD.mean().item()
+            lossG_recon = criterion_gan(outD, False, True)
+
+            lossG = lossG_prior + lossG_recon + loss_similarity
+            lossG.backward()
+            optG.step()
+
+            optD.zero_grad()
+            # Discriminator (Real)
+            outD = netD(x_real)
+            Dx1 = outD.mean().item()
+            lossD_real = criterion_gan(outD, True)
+            lossD_real.backward()
+
+            # Discriminator (Fake)
+            outD = netD(x_prior.detach())
+            lossD_fake = criterion_gan(outD, False)
+            lossD_fake.backward()
+
+            # Discriminator (Recon)
+            outD = netD(x_recon.detach())
+            lossD_recon = criterion_gan(outD, False)
+            lossD_recon.backward()
+            lossD = lossD_real + lossD_fake + lossD_recon
+            optD.step()
+
+            if i % 50 == 0:
+                # Reconstruction from latent code
+                outG = netG(fixed_noise).detach()
+                z_enc = netE(outG).detach()
+                reconG = netG(z_enc).detach()
+                eval_dist = (fixed_noise - z_enc).norm()
+
+                print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f Loss_E: %.4f Loss_Sim: %.4f D(x): %.4f  D(G(z)): %.4f / %.4f | Dist: %.4f'
+                    % (epoch + 1, args.n_epochs, i, len(dataloader), lossD.item(), lossG.item(), lossE.item(), loss_similarity.item(), Dx1, Dgz1, Dgz2, eval_dist))
+                save_image(torch.cat([outG, reconG], dim=0), '%s/fake/epoch%03d_%04d.jpg' % (args.result_path, epoch + 1, i + 1), normalize=True)
+
+                # Reconstruction from real image
+                z_enc = netE(fixed_real).detach()
+                reconG = netG(z_enc).detach()
+                save_image(torch.cat([fixed_real, reconG], dim=0), '%s/real/epoch%03d_%04d.jpg' % (args.result_path, epoch + 1, i + 1), normalize=True)
+
+        save_model((netG, netD, netE), (optG, optD, optE), epoch, args.checkpoint_path)
+
+
+def save_model(models, optimizers, epoch, checkpoint_path):
+    netG, netD, netE = models
+    optG, optD, optE = optimizers
+
+    checkpoint = {
+        'state_dict': {
+            'generator': netG.state_dict(),
+            'discriminator': netD.state_dict(),
+            'encoder': netE.state_dict(),
+        },
+        'optimizer': {
+            'generator': optG.state_dict(),
+            'discriminator': optD.state_dict(),
+            'encoder': optE.state_dict(),
+        },
+        'epoch': epoch,
+    }
+
+    torch.save(checkpoint, '%s/checkpoint_%03d.pth' % (checkpoint_path, epoch + 1))
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args)
---
+++ models/vaegan.py
@@ -0,0 +1,258 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def G_arch(ngf=64, img_dim=3):
+    arch = {}
+    arch[32] = {
+        'in_channels': [ngf * item for item in [8, 4]],
+        'out_channels': [ngf * item for item in [4, 2]],
+    }
+    arch[64] = {
+        'in_channels': [ngf * item for item in [16, 8, 4]],
+        'out_channels': [ngf * item for item in [8, 4, 2]],
+    }
+    arch[128] = {
+        'in_channels': [ngf * item for item in [16, 8, 4, 2]],
+        'out_channels': [ngf * item for item in [8, 4, 2, 1]],
+    }
+    return arch
+
+class Generator(nn.Module):
+    def __init__(self, z_dim=100, ngf=64, img_dim=3, resolution=64, bottom_width=4, init='N02', skip_init=False):
+        super().__init__()
+        self.z_dim = z_dim
+        self.ngf = ngf
+        self.img_dim = img_dim
+        self.resolution = resolution
+        self.bottom_width = bottom_width
+        self.init = init
+        self.arch = G_arch(ngf=ngf, img_dim=img_dim)[resolution]
+
+        self.linear = nn.Linear(z_dim, self.arch['in_channels'][0] * (bottom_width**2))
+        
+        self.blocks = nn.ModuleList()
+        for idx in range(len(self.arch['in_channels'])):
+            self.blocks.append(nn.Sequential(
+                nn.ConvTranspose2d(self.arch['in_channels'][idx], self.arch['out_channels'][idx], 4, stride=2, padding=1),
+                nn.BatchNorm2d(self.arch['out_channels'][idx]),
+                nn.ReLU(True)
+            ))
+        self.out_layer = nn.Sequential(
+            nn.ConvTranspose2d(self.arch['out_channels'][-1], img_dim, 4, stride=2, padding=1),
+            nn.Tanh()
+        )
+
+        if not skip_init:
+            self.init_weights()
+
+    def forward(self, z):
+        h = self.linear(z)
+        h = h.view(h.size(0), -1, self.bottom_width, self.bottom_width)
+
+        for idx, block in enumerate(self.blocks):
+            h = block(h)
+        out = self.out_layer(h)
+
+        return out
+
+    def init_weights(self):
+        self.param_count = 0
+        for module in self.modules():
+            if (isinstance(module, nn.ConvTranspose2d) 
+            or isinstance(module, nn.Linear)):
+                if self.init == 'ortho':
+                    nn.init.orthogonal_(module.weight)
+                elif self.init == 'N02':
+                    nn.init.normal_(module.weight, 0, 0.02)
+                elif self.init in ['glorot', 'xavier']:
+                    nn.init.xavier_uniform_(module.weight)
+                else:
+                    print('Init style not recognized...')
+            # if (isinstance(module, nn.BatchNorm2d)):
+            #     nn.init.normal_(module.weight, 1.0, 0.02)
+            
+            self.param_count += sum([p.data.nelement() for p in module.parameters()])
+        print('Param count for G''s initialized parameters: %d' % self.param_count)
+
+def D_arch(ndf=64, img_dim=3):
+    arch = {}
+    arch[32] = {
+        'in_channels': [img_dim] + [ndf * item for item in [2, 4]],
+        'out_channels': [ndf * item for item in [2, 4, 8]],
+    }
+    arch[64] = {
+        'in_channels': [img_dim] + [ndf * item for item in [2, 4, 8]],
+        'out_channels': [ndf * item for item in [2, 4, 8, 16]],
+    }
+    arch[128] = {
+        'in_channels': [img_dim] + [ndf * item for item in [2, 4, 8, 16]],
+        'out_channels': [ndf * item for item in [2, 4, 8, 16, 16]],
+    }
+    return arch
+
+class Discriminator(nn.Module):
+    def __init__(self, z_dim=100, ndf=64, img_dim=3, resolution=64, output_dim=1, init='N02', skip_init=False):
+        super().__init__()
+        self.ndf = ndf
+        self.img_dim = img_dim
+        self.resolution = resolution
+        self.init = init
+        self.arch = D_arch(ndf=ndf, img_dim=img_dim)[resolution]
+
+        self.blocks = nn.ModuleList()
+        for idx in range(len(self.arch['in_channels'])):
+            block = []
+            block.append(nn.Conv2d(self.arch['in_channels'][idx], self.arch['out_channels'][idx], 4, stride=2, padding=1))
+            if idx != 0:
+                block.append(nn.BatchNorm2d(self.arch['out_channels'][idx]))
+            block.append(nn.LeakyReLU(0.2, True))
+            self.blocks.append(nn.Sequential(*block))
+        
+        last_hidden = self.arch['out_channels'][-1]
+        self.fc = nn.Sequential(
+            nn.Linear(last_hidden, last_hidden),
+            nn.BatchNorm1d(last_hidden),
+            nn.LeakyReLU(0.2, True)
+        )
+        self.out_layer = nn.Linear(last_hidden, output_dim)
+
+        if not skip_init:
+            self.init_weights()
+
+    def forward(self, x, out_hidden=False):
+        h = x
+        for idx, block in enumerate(self.blocks):
+            h = block(h)
+        if out_hidden:
+            out_h = h
+        
+        h = torch.sum(h, dim=[2, 3]) # Global sum pooling
+        h = self.fc(h)
+        out = self.out_layer(h)
+
+        if out_hidden:
+            return out, out_h
+        else:
+            return out
+
+    def init_weights(self):
+        self.param_count = 0
+        for module in self.modules():
+            if (isinstance(module, nn.Conv2d) 
+            or isinstance(module, nn.Linear)):
+                if self.init == 'ortho':
+                    nn.init.orthogonal_(module.weight)
+                elif self.init == 'N02':
+                    nn.init.normal_(module.weight, 0, 0.02)
+                elif self.init in ['glorot', 'xavier']:
+                    nn.init.xavier_uniform_(module.weight)
+                else:
+                    print('Init style not recognized...')
+            # if (isinstance(module, nn.BatchNorm2d)):
+            #     nn.init.normal_(module.weight, 1.0, 0.02)
+            
+            self.param_count += sum([p.data.nelement() for p in module.parameters()])
+        print('Param count for D''s initialized parameters: %d' % self.param_count)
+
+
+def E_arch(ndf=64, img_dim=3):
+    arch = {}
+    arch[32] = {
+        'in_channels': [img_dim] + [ndf * item for item in [2, 4]],
+        'out_channels': [ndf * item for item in [2, 4, 8]],
+    }
+    arch[64] = {
+        'in_channels': [img_dim] + [ndf * item for item in [2, 4, 8]],
+        'out_channels': [ndf * item for item in [2, 4, 8, 16]],
+    }
+    arch[128] = {
+        'in_channels': [img_dim] + [ndf * item for item in [2, 4, 8, 16]],
+        'out_channels': [ndf * item for item in [2, 4, 8, 16, 16]],
+    }
+    return arch
+
+class Encoder(nn.Module):
+    def __init__(self, ndf=64, img_dim=3, resolution=64, output_dim=100, init='N02', skip_init=False):
+        super().__init__()
+        self.ndf = ndf
+        self.img_dim = img_dim
+        self.resolution = resolution
+        self.init = init
+        self.arch = D_arch(ndf=ndf, img_dim=img_dim)[resolution]
+
+        self.blocks = nn.ModuleList()
+        for idx in range(len(self.arch['in_channels'])):
+            self.blocks.append(nn.Sequential(
+                nn.Conv2d(self.arch['in_channels'][idx], self.arch['out_channels'][idx], 4, stride=2, padding=1),
+                nn.BatchNorm2d(self.arch['out_channels'][idx]),
+                nn.ReLU(True)
+            ))
+
+        last_hidden = self.arch['out_channels'][-1]
+        self.fc = nn.Sequential(
+            nn.Linear(last_hidden, last_hidden),
+            nn.BatchNorm1d(last_hidden),
+            nn.ReLU(True)
+        )
+        self.out_mu = nn.Linear(last_hidden, output_dim)
+        self.out_logvar = nn.Linear(last_hidden, output_dim)
+
+        if not skip_init:
+            self.init_weights()
+
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        out = self.reparameterize(mu, logvar)
+
+        return out
+
+    def reparameterize(self, mu, logvar):
+        std = F.softplus(logvar)
+        eps = torch.randn_like(std)
+        return mu + eps*std
+
+    def encode(self, x):
+        h = x
+        for idx, block in enumerate(self.blocks):
+            h = block(h)
+        h = torch.sum(h, dim=[2, 3]) # Global sum pooling
+        h = self.fc(h)
+
+        mu = self.out_mu(h)
+        logvar = self.out_logvar(h)
+        return mu, logvar
+
+    def init_weights(self):
+        self.param_count = 0
+        for module in self.modules():
+            if (isinstance(module, nn.Conv2d) 
+            or isinstance(module, nn.Linear)):
+                if self.init == 'ortho':
+                    nn.init.orthogonal_(module.weight)
+                elif self.init == 'N02':
+                    nn.init.normal_(module.weight, 0, 0.02)
+                elif self.init in ['glorot', 'xavier']:
+                    nn.init.xavier_uniform_(module.weight)
+                else:
+                    print('Init style not recognized...')
+            # if (isinstance(module, nn.BatchNorm2d)):
+            #     nn.init.normal_(module.weight, 1.0, 0.02)
+            
+            self.param_count += sum([p.data.nelement() for p in module.parameters()])
+        print('Param count for E''s initialized parameters: %d' % self.param_count)
+
+if __name__ == "__main__":
+    netG = Generator()
+    netD = Discriminator()
+    netE = Encoder()
+
+    z = torch.randn(4, 100)
+    fake = netG(z)
+    logits = netD(fake, z)
+    print(fake.shape)
+    print(logits.shape)
+    enc_z = netE(fake)
+    logits_e = netD(fake, enc_z)
+    print(enc_z.shape)
+    print(logits_e.shape)
